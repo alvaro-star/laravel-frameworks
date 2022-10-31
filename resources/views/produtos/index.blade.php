0079edb60bf1f2e05ee52c20743ee9a47b286520
ALVARO VARGAS ALVAREZ  TecInf <07884569116@laboratorio.ifms.br>
2022-10-31 10:15:22 -0400

    Troca do foreach por foreles na index da paginas produtos

--- resources/views/produtos/index.blade.php
+++ resources/views/produtos/index.blade.php
@@ -5,11 +5,11 @@
 <script src="https://cdn.tailwindcss.com"></script>
 @endsection
 @section('alerta')
-    @if(isset($alerta))
-        <div class = "alerta rounded bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white text-center mx-12 py-2.5">
-            {{$alerta}}
-        </div>
-    @endif
+@if(isset($alerta))
+<div class="alerta rounded bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 text-white text-center mx-12 py-2.5">
+    {{$alerta}}
+</div>
+@endif
 
 @endsection
 
@@ -24,42 +24,46 @@
     <button class="">Eliminar</button>
 </form>
 @endif
-<a href="/produtos/create" class="my-2 mx-2 align-items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-1/2 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</a>
-@endsection
+<a href=" /produtos/create" class="my-2 mx-2 align-items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-1/2 sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Cadastrar</a>
+    @endsection
 
-@section('content')
-<div class="produtos flex grid gap-y-8 place-content-start gap-x-10 sm:grid-cols-2 sm:gap-x-10 md:grid-cols-3 md:gap-x-8 lg:grid-cols-4 lg:gap-x-14 xl:grid-cols-5 xl:gap-10">
-    @foreach($produtos as $produto)
+    @section('content')
+    <div class="produtos flex grid gap-y-8 place-content-start gap-x-10 sm:grid-cols-2 sm:gap-x-10 md:grid-cols-3 md:gap-x-8 lg:grid-cols-4 lg:gap-x-14 xl:grid-cols-5 xl:gap-10">
+        @forelse($produtos as $produto)
 
-    <div class="w-56 produto text-center rounded bg-gradient-to-r from-cyan-500 to-blue-500 py-2 px-2 text-white">
-        <h4 class="text-2xl">{{$produto->nome}}</h4>
-        <img class="rounded mt-7 h-40 w-auto mx-auto" src="{{$produto->url}}" alt="imagem do produto">
+        <div class="w-56 produto text-center rounded bg-gradient-to-r from-cyan-500 to-blue-500 py-2 px-2 text-white">
+            <h4 class="text-2xl">{{$produto->nome}}</h4>
+            <img class="rounded mt-7 h-40 w-auto mx-auto" src="{{$produto->url}}" alt="imagem do produto">
 
-        <div>
-            <p class="font-bold">Preco: {{$produto->preco}}</p>
-            <p>Marca: {{$produto->marca}}</p>
-            <p>Categoria: {{$produto->categorias_id->nome}}</p>
-            <p class="">Descricao: {{$produto->descricao}}</p>
+            <div>
+                <p class="font-bold">Preco: {{$produto->preco}}</p>
+                <p>Marca: {{$produto->marca}}</p>
+                <p>Categoria: {{$produto->categorias_id->nome}}</p>
+                <p class="">Descricao: {{$produto->descricao}}</p>
+            </div>
+            <div class="flex justify-center text-white pb-5">
+                <a class="my-2 mx-5 align-items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-1/2 sm:w-auto px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="{{route('produtos.show', ['produto' => $produto])}}"> Ver </a>
+            </div>
         </div>
-        <div class="flex justify-center text-white pb-5">
-            <a class="my-2 mx-5 align-items-center text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded text-sm w-1/2 sm:w-auto px-10 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" href="{{route('produtos.show', ['produto' => $produto])}}"> Ver </a>
+        @empty
+        <div class="flex justify-center items-center">
+            <p class="h-64 font-bold">Nao há nenhuma categoria</p>
         </div>
+        @endforelse
     </div>
-    @endforeach
-</div>
 
-@endsection
+    @endsection
 
-@section('css')
-<style>
-    .produtos{
-        margin-left: 5%;
-        margin-right: 5%;
-    }
+    @section('css')
+    <style>
+        .produtos {
+            margin-left: 5%;
+            margin-right: 5%;
+        }
 
-    .alerta{
-        margin-left: 5%;
-        margin-right: 5%;
-    }
-</style>
-@endsection
+        .alerta {
+            margin-left: 5%;
+            margin-right: 5%;
+        }
+    </style>
+    @endsection
